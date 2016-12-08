5febb5cbfbb1770ef4faf7cff476f1464287ce8b
Oleg Konyk <okonyk@apple.com>
2016-12-08 10:43:22 -0800

    throwing error on mongo connection error

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,7 +7,11 @@ mongoose.Promise = global.Promise;
 module.exports = function(config) {
   mongoose.connect(config.db);
   var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error...'));
+  db.on('error', function(err) {
+    console.log(err);
+    throw err;
+    // console.error.bind(console, 'connection error...')
+  });
   db.once('open', function callback() {
     console.log('eduplat db opened');
     User.createDefaultUsers();
